9faaf1e2672834076e6bca4a4887c2c390a98fd5
Walter Higgins <walter.higgins@gmail.com>
2014-01-21 21:28:35 +0000

    Update Example1.ino

--- sketches/Example1.ino
+++ sketches/Example1.ino
@@ -2,6 +2,32 @@
    Based on the DigitalReadSerial example provided by Arduino.
    This is a slight modification. If the button state is changed from LOW to HIGH
    then the text 'BUTTON PRESSED' is emitted over Serial connection.
+   
+   Use the schematic/circuit suggested here http://arduino.cc/en/Tutorial/DigitalReadSerial
+   
+   You'll need to install mosquitto or another MQTT broker.
+   
+   1. Start the mosquitto broker (using default host/port localhost:1883)
+   2. Pipe the serial output through mqtt like this...
+   
+      tail -f /dev/tty.usbmodemfd131 | mosquitto_pub -t arduino -l
+
+   3. Start up Craftbukkit like this...
+       
+      java -cp sc-mqtt.jar:craftbukkit.jar org.bukkit.craftbukkit.Main
+
+   4. At the server console prompt type
+   
+      js var mqtt = require('sc-mqtt'), client = mqtt.client();
+      js client.connect();
+      js client.subscribe('arduino');
+      js world = server.getWorld('world');
+      js client.onMessageArrived(function(topic,message){ var time = world.time+12000; world.time=time });
+      
+   5. Launch the minecraft client and join the server in multi-player mode.
+   
+   6. Press the pushbutton to advance time by 1/2 a day.
+   
  */
 
 // digital pin 2 has a pushbutton attached to it. Give it a name:
