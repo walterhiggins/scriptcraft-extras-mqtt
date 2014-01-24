efb092ec972aea52bac22fec9c8e44e05fb8efed
walterhiggins <walter.higgins@gmail.com>
2014-01-24 23:40:53 +0000

    changed example1

--- sketches/example-1/Example1.ino
+++ sketches/example-1/Example1.ino
@@ -31,29 +31,28 @@
  */
 
 // digital pin 2 has a pushbutton attached to it. Give it a name:
-int pushButton = 2;
+int PUSHBUTTON = 2;
 int previousState = LOW;
 int currentState = LOW;
 // the setup routine runs once when you press reset:
 void setup() {
-  // initialize serial communication at 9600 bits per second:
-  Serial.begin(9600);
-  // make the pushbutton's pin an input:
-  pinMode(pushButton, INPUT);
+    // initialize serial communication at 9600 bits per second:
+    Serial.begin(9600);
+    // make the pushbutton's pin an input:
+    pinMode(PUSHBUTTON, INPUT);
 }
 
 // the loop routine runs over and over again forever:
 void loop() {
-  // read the input pin:
-  currentState = digitalRead(pushButton);
-  if (currentState != previousState){
-    if (currentState == HIGH)
-        Serial.println("BUTTON PRESSED");
-    previousState = currentState;
-  }
-  // print out the state of the button:
-  
-  delay(20);        // delay in between reads for stability
+    // read the input pin:
+    currentState = digitalRead(PUSHBUTTON);
+    if (currentState != previousState){
+        if (currentState == HIGH){
+            Serial.println("BUTTON PRESSED");
+        }       
+        previousState = currentState;
+    }
+    delay(20);        // delay in between reads for stability
 }
 
 
